47fafbcb87723f344e158c2ff9a588c626c5e862
Richard Danis <richard0135@gmail.com>
2018-07-03 16:12:30 +0200

    add h2

--- src/main/java/com/first_project/entity/User.java
+++ src/main/java/com/first_project/entity/User.java
@@ -1,10 +1,27 @@
 package com.first_project.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
     private String firstName;
     private String secondName;
 
     public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
---
+++ src/main/java/com/first_project/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.first_project.repository;
+
+import com.first_project.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+}
