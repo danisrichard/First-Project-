eaf4047ab3583331391738f982eff67ebddbb8d7
Richard Danis <richard0135@gmail.com>
2018-07-03 16:00:52 +0200

    updates

--- src/main/java/com/first_project/service/Impl/UserServiceImpl.java
+++ src/main/java/com/first_project/service/Impl/UserServiceImpl.java
@@ -2,10 +2,12 @@ package com.first_project.service.Impl;
 
 import com.first_project.entity.User;
 import com.first_project.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private List<User> userList = new ArrayList<>();
